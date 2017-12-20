c4877250daf80007554594f9107ff55a69f3845b
cosskun <34624521+cosskun@users.noreply.github.com>
2017-12-20 09:27:13 +0300

    Add files via upload

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.WriteLine("Adınızı Giriniz:");ad = Console.ReadLine(); 
+            foreach(char isim in ad)
+            Console.Write((char)(isim+10));
+            Console.ReadKey();
+        }
+    }
+}
